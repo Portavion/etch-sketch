e93e2cd2dca3a87a0bd04f41477a766239ffe87b
Portavion <156545430+Portavion@users.noreply.github.com>
2024-08-18 14:05:56 +0200

    Add eventListener and logic to colour hovered tiles in black

--- script.js
+++ script.js
@@ -4,10 +4,31 @@ function createGrid (resolution)  {
     
     for (let i = 0; i< resolution; i++) {
         pixels.push(document.createElement('div'));
-        pixels[i].className = 'pixel';
+        pixels[i].className = 'pixel';``
+        pixels[i].id = i+1;
         pixels[i].style.flex = `1 1 ${(100/Math.sqrt(resolution)-1)}%`
-        container.appendChild(pixels[i]);``
+        container.appendChild(pixels[i]);
     }
 }
 
-createGrid(16);
+function changeColor(target){
+    let id = target.id;
+    overedSquare = document.getElementById(id);
+    if (overedSquare != null) {
+        overedSquare.style.backgroundColor = 'black';
+    }
+}
+
+function createEventHover (){
+    let container = document.querySelector('.container');
+    
+    container.addEventListener('mouseover', (event) => {
+        let target = event.target;
+        changeColor(target);
+    });
+}
+
+
+
+createGrid(49);
+createEventHover();
